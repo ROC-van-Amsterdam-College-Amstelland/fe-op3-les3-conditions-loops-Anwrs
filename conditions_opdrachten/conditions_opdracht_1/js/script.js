dc659009701e6d00f6a6634b7f686c14e962730c
anouar el ghazaoui <anouar.eg@live.nl>
2019-11-26 11:14:39 +0100

    gfgf

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,7 @@
 //javascript code
+
+var banksaldo = 10;
+
+if (banksaldo < 25) {
+    alert("Sorry, je hebt niet genoeg saldo");
+}
--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,19 @@
 //javascript code
+function checkSaldo() {
+
+
+var geldOpname = document.getElementById("textveld").value;
+
+var bankSaldo = 1000;
+var restSaldo = 1000 - geldOpname;
+
+if (restSaldo >= 25) {
+    alert("Je hebt voldoende saldo om dit bedrag op te nemen.")
+
+}
+
+if (restSaldo < 25) {
+    alert("Sorry, je hebt te weinig saldo!");
+}
+
+}
