1e0ef4904be862affac75aebadc33cd1e4491ee5
anouar el ghazaoui <anouar.eg@live.nl>
2019-11-26 12:20:22 +0100

    bezig

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,25 @@
 //javascript code
+
+function checkSaldo() {
+
+
+    var geldOpname = document.getElementById("textveld").value;
+    
+    var bankSaldo = 1000;
+    var restSaldo = 1000 - geldOpname;
+    
+    
+    if (restSaldo == 500) {
+        alert("Er is een bedrag op de bank gestort.")
+    
+    }
+    
+    if (restSaldo > 500) {
+        alert("Er is een bedrag en een bonus op de bank gestort.");
+    }
+
+    if (restSaldo < 500) {
+        alert("Er is geen bedrag gestort.")
+    }
+    
+    }
