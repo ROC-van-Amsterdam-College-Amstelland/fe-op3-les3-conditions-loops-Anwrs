b6cddcbb177492b89091e0341aff99f06fa0f941
anouar el ghazaoui <anouar.eg@live.nl>
2019-12-03 10:19:32 +0100

    klaar

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,5 @@
 //javascript code
+
+var myNumber = 4;
+
+alert("De variabele myNumber heeft de waarde: " + myNumber);
--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,19 @@
 //javascript code
+
+var myOperator = "+";
+var myNumber1 = prompt("Geef een getal op");
+var myNumber2 = prompt("Geef een ander getal op");
+var resultaat;
+
+if (myOperator == "+" ) {
+
+        console.log("De operator is +");
+        resultaat = Number(myNumber1) + Number(myNumber2);
+        document.getElementById("resultaat").innerHTML = resultaat;
+        
+    }else if(myOperator == "-" ){
+        
+        console.log("De operator is -");
+        resultaat = Number(myNumber1) - Number(myNumber2);
+        document.getElementById("resultaat").innerHTML = resultaat;
+}
