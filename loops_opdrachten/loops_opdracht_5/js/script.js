d31b34102ea9cb98b1030ca48ff484e1fc5a61b1
anouar el ghazaoui <anouar.elghazaoui@student.rocva.nl>
2019-12-05 15:34:22 +0100

    done

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,17 @@
-//javascript code
+ for (var teller = 0; teller <= 20; teller = teller + 1){
+
+
+    if(teller % 2 == 0){
+
+        
+        document.getElementById("resultaat").innerHTML = document.getElementById("resultaat").innerHTML + "<h6>" + teller ;
+    }
+    
+    else{
+        
+        console.log(teller);   
+        
+    }
+    
+    
+ }
